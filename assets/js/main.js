7ed4282d2cf2fdc206a2c5984862f5c05ce2c588
caroquintana <caro.quintana.n@gmail.com>
2017-06-18 03:09:12 -0400

    ej ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,18 +3,30 @@ var inputnombre = document.getElementById("name").value,
     inputcontra = document.getElementById("contrasena").value,
     inicioSesion = document.getElementById("boton");
 
+validacion = true;
+
 inicioSesion.addEventListener("click", validar);
 
 function validar(){
-	if(inputnombre == ""){
+	if(inputnombre === ""){
 		alert("Favor, ingrese nombre.");
-	}if(inputcontra == 123456){
+		// var pAlert= document.getElementById("alertname");
+		// pAlert.appendChild(document.createTextNode("Favor, ingrese nombre."));
+		// validacion = validacion && false;
+	}else if(inputcontra == 123456){
 		alert("Su contraseña no debe ser 123456.");
+		//var pAlert2 = document.getElementById("alertcontra");
+		//pAlert2.appendChild(document.createTextNode("Su contraseña no debe ser 123456."));
 	}if(inputcontra.length < 6){
-		alert("Su contraseña debe contener más de 6 dígitos.")
+		alert("Su contraseña debe contener a lo menos de 6 dígitos.")
+		//var pAlert3 = document.getElementById("alertcontra");
+		//pAlert3.appendChild(document.createTextNode("Su contraseña debe contener a lo menos de 6 dígitos."));
+	}else{
+		//return ("validaciones ok")
 	}
 };
 
+//me funciona con el dom pero no me reconoce el ok de las validaciones.
 
 
 
