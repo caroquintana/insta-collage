9c243699cad62461ae728977ddfb8689b0d3b6b7
caroquintana <caro.quintana.n@gmail.com>
2017-06-18 01:51:20 -0400

    validación con alert)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,33 @@
+// Validación
+var inputnombre = document.getElementById("name").value,
+    inputcontra = document.getElementById("contrasena").value,
+    inicioSesion = document.getElementById("boton");
+
+inicioSesion.addEventListener("click", validar);
+
+function validar(){
+	if(inputnombre == ""){
+		alert("Favor, ingrese nombre.");
+	}if(inputcontra == 123456){
+		alert("Su contraseña no debe ser 123456.");
+	}if(inputcontra.length < 6){
+		alert("Su contraseña debe contener más de 6 dígitos.")
+	}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Drag & Drop
 function drag(event) {
 	event.dataTransfer.setData("text", event.target.id);
 }
@@ -12,3 +42,4 @@ function drop(event) {
 	var dato = event.dataTransfer.getData("text");
 	event.target.appendChild(document.getElementById(dato));
 }
+
